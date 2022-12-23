e2e7b83cabfdde3502b396b6c62e3401dd64579b
Alexandre Blanchet <aldazar@alexandres-macbook-pro.home>
2022-12-23 11:32:59 +0000

    review code

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from posts.models import BlogPost
 # Register your models here.
-
+# TODO: Fix pylint warnings
 class BlogPostAdmnin(admin.ModelAdmin):
     list_display = ("title", "published", "created_on", "last_update",) #tous ce que nous voullons afficher dans l'interface d' admin
     list_editable = ("published",) #qui permete d'édité certain champ dans l'interface d'admin
